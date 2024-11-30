6af1422f3376e9af52888266f80f05c20c813df2
Nathan Cortez <nath.co03@icloud.com>
2024-11-30 12:58:43 -0500

    adds title screen

--- main_game.py
+++ main_game.py
@@ -7,7 +7,9 @@ class Game(tk.Tk):
     super().__init__()
     self.title("Tic-Tac-Toe 2")
 
-
+    self.title_screen_img = tk.PhotoImage(file='tic-tac-toe-2/assets/title_screen.png')
+    self.title_screen_img = self.title_screen_img.subsample(2,2)
+    tk.Label(self, image=self.title_screen_img).grid(row=0, column=0)
 
 
 def main():

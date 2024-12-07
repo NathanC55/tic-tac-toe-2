55f0c2466e238731710bbbb81ea159d4d59eff2f
Nathan Cortez <nath.co03@icloud.com>
2024-12-07 00:29:13 -0500

    adds board, moves code

--- main_game.py
+++ main_game.py
@@ -1,15 +1,17 @@
 import tkinter as tk
-
+from tkinter import messagebox
 class Game(tk.Tk):
-  def set_up_playground(self):
-    self.clear_content()
-    self.back_button = tk.Button(self, text="<", font='Arial, 25', command= self.set_up_title_screen)
-    self.back_button.grid(row = 0, column = 0)
+  def __init__(self):
+    super().__init__()
+    self.title("Tic-Tac-Toe 2")
+    self.geometry("800x400")
+    self.resizable(False, False)
 
-
-  def clear_content(self):
-    for widget in self.winfo_children():
-      widget.destroy()
+    self.current_player = "X"
+    self.board = [["" for _ in range(3)] for _ in range(3)]
+    self.buttons = [[None for _ in range(3)] for _ in range(3)]
+    
+    self.set_up_title_screen()
 
   def set_up_title_screen(self):
     self.clear_content()
@@ -21,18 +23,40 @@ class Game(tk.Tk):
     self.play_button.place(x=350, y=300)
 
 
-  def __init__(self):
-    super().__init__()
-    self.title("Tic-Tac-Toe 2")
-    self.geometry("800x400")
-    self.resizable(False, False)
+  def set_up_playground(self):
+    self.clear_content()
+    self.back_button = tk.Button(self, text="<", font='Arial, 25', command= self.set_up_title_screen)
+    self.back_button.grid(row = 0, column = 0)
 
-    self.set_up_title_screen()
+    self.game_content = tk.Frame(self)
+    self.game_content.grid(row=1, column=2, sticky="NSEW")
+    self.create_board()
 
+  def create_board(self):
+    for row in range(3):
+      for col in range(3):
+        button = tk.Button(self.game_content,text="",font=("Arial", 24),width=5,height=2, command=lambda r=row, c=col: self.make_move(r, c))
+        button.grid(row=row, column=col)
+        self.buttons[row][col] = button
+
+
+  def make_move(self, row, col):
+    if self.board[row][col] == "":
+      self.board[row][col] = self.current_player
+      self.buttons[row][col].config(text=self.current_player, state="disabled")
+
+    if self.current_player == "X":
+      self.current_player = "O" 
+      
+    else: 
+      self.current_player = "X"
+
+
+  def clear_content(self):
+    for widget in self.winfo_children():
+      widget.destroy()
 
     
-
-
 def main():
   Game().mainloop()
 
